d4a9b7eeaf107ca155d9329a0e616965d2a07078
Казюкова Ирина <KaziukovaIA@mpei.ru>
2017-05-09 21:01:29 +0300

    punkt 5.1

--- main.cpp
+++ main.cpp
@@ -9,4 +9,5 @@ int main() {
     cout << "A + B = " << a + b << '\n';
     cout << "A * 2 = " << a * 2 << '\n';
     cout << "A - B = " << a - b << '\n';
+    cout << "A * B = " << a * b << '\n';
 }
