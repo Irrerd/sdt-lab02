081d50a5b3dba1901829467bffe40e016cabcbcf
Казюкова Ирина <KaziukovaIA@mpei.ru>
2017-05-09 18:14:08 +0300

    punkt 3.1

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int a, b;
     cout << "Enter A and B; ";
-    cout << "A + B = " << a + b << '\n';
     cin >> a >> b;
+    cout << "A + B = " << a + b << '\n';
+    cout << "A * 2 = " << a * 2 << '\n';
 }
