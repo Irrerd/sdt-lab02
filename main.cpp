61d2529a8eec0cdf32d6034ff61eebeccf0e625d
Казюкова Ирина <KaziukovaIA@mpei.ru>
2017-05-09 17:16:05 +0300

    punkt 1.6.4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a, b;
+    cout << "Enter A and B; ";
+    cin >> a >> b;
 }
